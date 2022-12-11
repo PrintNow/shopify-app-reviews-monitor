1f5cae0376ce9390b3ae4a1bfbe290eb2392f00e
Chuwen <i@nowtime.cc>
2022-12-11 12:18:36 +0800

    pref: 优化时区显示

--- app/Services/Shopify/AppReviewsNotifyService.php
+++ app/Services/Shopify/AppReviewsNotifyService.php
@@ -17,7 +17,9 @@ class AppReviewsNotifyService
         if ($this->shopifyAppReview->posted_at !== null) {
             $date = "**Posted at**: {$this->shopifyAppReview->posted_at}";
         }
-        $discoverTime = $this->shopifyAppReview->created_at?->format('Y-m-d H:i:s') ?? '';
+        $discoverTime = $this->shopifyAppReview->created_at
+            ?->setTimezone('Asia/Shanghai')
+            ->format('Y-m-d H:i:s') ?? '';
 
         Factory::dingTalk()
             ->setToken(config('notify.dingtalk.token'))
@@ -25,19 +27,19 @@ class AppReviewsNotifyService
             ->setMessage(new \Guanguans\Notify\Messages\DingTalk\MarkdownMessage([
                 'title' => "{$this->shopifyAppReview->rating}-star & {$this->shopifyAppReview->slug} | {$this->shopifyAppReview->name}",
                 'text' => <<<MARKDOWN
-## {$this->shopifyAppReview->slug}
----
-#### **Customer**: {$this->shopifyAppReview->name}
-#### **Reviews rating**: {$this->shopifyAppReview->rating}-star
-#### {$date}
----
-#### **Comment**: {$this->shopifyAppReview->comment}
-#### **Discover time**: {$discoverTime}
+                            ## {$this->shopifyAppReview->slug}
+                            ---
+                            #### **Customer**: {$this->shopifyAppReview->name}
+                            #### **Reviews rating**: {$this->shopifyAppReview->rating}-star
+                            #### {$date}
+                            ---
+                            #### **Comment**: {$this->shopifyAppReview->comment}
+                            #### **Discover time**: {$discoverTime}
 
----
+                            ---
 
-[Click to view the reviews page](https://apps.shopify.com/{$this->shopifyAppReview->slug})
-MARKDOWN
+                            [Click to view the reviews page](https://apps.shopify.com/{$this->shopifyAppReview->slug})
+                          MARKDOWN
                 ,
             ]))
             ->send();
